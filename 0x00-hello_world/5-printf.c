f3b0a2cb93c5346422138ca5f7048dfd5c685768
WanjikuSharon <sharonwanjiku292@gmail.com>
2022-07-02 10:09:55 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * My hello, printf task
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+	return(0);
 }	
  
