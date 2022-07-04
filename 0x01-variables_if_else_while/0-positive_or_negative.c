b12263c7e8129abf6cc69d8b13df47cb35fd536f
WanjikuSharon <sharonwanjiku292@gmail.com>
2022-07-04 11:02:55 -0700

    Zero positive and negative to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,14 @@
 /**
  * Main-Entry point (No argument)
  *
- * Description - Assigning a variable positive or negative
+ * Description - assigning a random number to the variable n each time it is executed
+ *
+ * If number is greater than 0: its positive
+ *
+ * If number equals to 0: its zero
+ *
+ * If number is less than 0: its negative
+ *
  *Return: Always 0(Success)
  */
 int main(void)
@@ -14,11 +21,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ( n==0 )
-		printf("n is zero: %d\n", n);
-	if ( n>0 )
-		printf("n is positive\n");
-	if ( n<0 )
-		printf("n is negative\n");
+	if ( n > 0 ) {
+		printf("%d is positive\n", n);
+	}
+	else if ( n < 0 ) {
+		printf("%d is negative\n", n);
+	}
+	else {
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
