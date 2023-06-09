2dc106efac641f9e77a087425a9ed2668334a76d
WanjikuSharon <sharonwanjiku292@gmail.com>
2023-06-09 04:10:54 -0700

    Number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * Return: 0 - success.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("Number of arguments: %d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
