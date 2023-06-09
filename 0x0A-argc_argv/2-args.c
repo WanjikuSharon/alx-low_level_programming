d0f5f1498bc7d2501aac4efb1f362ad5a22837f7
WanjikuSharon <sharonwanjiku292@gmail.com>
2023-06-09 04:55:27 -0700

    Updates

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,11 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-
-		return (0);
+	return (0);
 }
